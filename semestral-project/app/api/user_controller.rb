b5c8272f8cc26eda94099861982e072842b3997b
Jan Štefánik <stefaja7@cvut.cz>
2022-01-19 11:29:46 +0100

    Accept content-type:json in user controller

--- semestral-project/app/api/user_controller.rb
+++ semestral-project/app/api/user_controller.rb
@@ -1,3 +1,9 @@
+require 'json'
+
+before do
+  content_type :json
+end
+
 class UserController < Sinatra::Base
   get '/user' do
     "user controller"
@@ -7,4 +13,13 @@ class UserController < Sinatra::Base
     header_test = request.env['HTTP_USER_AGENT']
     "#{params[:testVar]} - path variable test | #{header_test} - header test"
   end
+
+ # # json test
+ # post '/user' do
+ #   # content_type :json
+ #   parsedBody = JSON.parse(request.body.read)
+ #   puts "I got some JSON: #{parsedBody.inspect}"
+ #   puts "#{parsedBody['test']}"
+ #   parsedBody.to_json
+ # end
 end
