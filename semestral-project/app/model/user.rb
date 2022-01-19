34fa5661429d641759fc1d84b82b2bccd73f199d
Jan Štefánik <stefaja7@cvut.cz>
2022-01-19 15:30:08 +0100

    Add accidentaly reverted changes - bcrypt in user model, db port in config and user dto parsing

--- semestral-project/app/model/user.rb
+++ semestral-project/app/model/user.rb
@@ -1,5 +1,17 @@
 require 'active_record'
+require 'bcrypt'
 
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :quiz, :dependent => :destroy
+
+  def password
+    @password ||= Password.new(hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hash = @password
+  end
 end
--- semestral-project/app/service/user_service.rb
+++ semestral-project/app/service/user_service.rb
@@ -1,10 +1,20 @@
 require_relative '../api/dto/user_dto'
+require_relative '../model/user'
 
 class UserService
   def persist_user(request)
     parsed_request = JSON.parse(request.body.read)
+    user_dto = validate_dto(parsed_request)
+    user = User.create(name: user_dto['username'], password: user_dto['password'])
+    user.to_json
+    #return result.to_h if result.success?
+    # raise ArgumentError.new("Invalid request body #{result.errors.to_h}")
+  end
+
+  private
+  def validate_dto(dto)
     user_dto_validation = UserDto.new
-    result = user_dto_validation.call(parsed_request)
+    result = user_dto_validation.call(dto)
     return result.to_h if result.success?
     raise ArgumentError.new("Invalid request body #{result.errors.to_h}")
   end
